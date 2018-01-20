5d9bbd86608ec1b758c91d882b712bea6588d89c
zpjjimmie <992957086@qq.com>
2018-01-20 20:05:29 +0800

    优化后的冒泡排序

---
+++ BubbleSort.h
@@ -0,0 +1,92 @@
+#pragma once
+
+/*
+	冒泡排序:
+			使用鸡尾酒排序法，正序和反序各遍历一次；
+			如果一次遍历中没有交换位置，则排序完成
+*/
+
+template<typename T>
+class BubbleSort
+{
+	typedef std::vector<T> SORTLIST;
+
+public:
+	BubbleSort(unsigned int max = 10000) : m_max(max) { }
+
+	void init(SORTLIST& t);
+	void sort();
+	void print();
+
+	bool isMax() { return m_sortlist.size() >= m_max; }
+
+private:
+	SORTLIST m_sortlist;
+	unsigned int m_max;
+};
+
+template<typename T>
+void BubbleSort<T>::init(SORTLIST& t)
+{
+	if (isMax())
+		return;
+
+	m_sortlist = t;
+	return;
+}
+
+template<typename T>
+void BubbleSort<T>::sort()
+{
+	int len = m_sortlist.size();
+	if (len <= 1)
+		return;
+
+	int left = 0, right = len - 1;
+
+	bool swap_flag = false;
+	while (left < right)
+	{
+		swap_flag = false;
+		for (int i = left; i < right; ++i)
+		{
+			if (m_sortlist[i + 1] < m_sortlist[i])
+			{
+				std::swap(m_sortlist[i], m_sortlist[i + 1]);
+				swap_flag = true;
+			}
+		}
+
+		if (!swap_flag)
+			return;
+
+		--right;
+
+		swap_flag = false;
+		for (int i = right; i > left; --i)
+		{
+			if (m_sortlist[i] < m_sortlist[i - 1])
+			{
+				std::swap(m_sortlist[i], m_sortlist[i - 1]);
+				swap_flag = true;
+			}
+		}
+
+		if (!swap_flag)
+			return;
+
+		++left;
+	}
+}
+
+template<typename T>
+void BubbleSort<T>::print()
+{
+	for (size_t i = 0; i < m_sortlist.size(); ++i)
+	{
+		if (i % 10 == 0)
+			std::cout << std::endl;
+		std::cout << m_sortlist[i] << "\t";
+	}
+	std::cout << "\n" << std::endl;
+}
--- unittest.cpp
+++ unittest.cpp
@@ -2,6 +2,7 @@
 #include "QuickSort.h"
 #include "MergeSort.h"
 #include "InsertSort.h"
+#include "BubbleSort.h"
 
 void test_heapsort()
 {
@@ -65,10 +66,20 @@ void test_insertsort()
 	insertsort.print();
 }
 
+void test_bubblesort()
+{
+	std::vector<int> sortlist = { 1,5,3,100,3,2,5,6,7,8,19,10 };
+	BubbleSort<int> bubblesort(100);
+	bubblesort.init(sortlist);
+	bubblesort.sort();
+	bubblesort.print();
+}
+
 int main()
 {
 	//test_heapsort();
 	//test_quicksort();
 	//test_mergesort();
-	test_insertsort();
+	//test_insertsort();
+	test_bubblesort();
 }
